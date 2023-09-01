608adb718131660cceb7ff57c453c46b07dd47a5
Lamanxo <smgbysgm@gmail.com>
2023-09-01 16:30:22 +0300

    progress with users

---
+++ src/main/java/com/example/ShareTools/model/User.java
@@ -0,0 +1,97 @@
+package com.example.ShareTools.model;
+
+import com.example.ShareTools.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+    @Column(name = "email")
+    String email;
+    @Column(name = "name")
+    String name;
+    @Column(name = "phonenumber")
+    String phoneNumber;
+    @Column(name = "isactive")
+    boolean isActive;
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "image_id")
+    Image avatar;
+    @Column(name = "password", length = 1000)
+    String password;
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    Set<Role> roles = new HashSet<>();
+    @Column(name = "createdate")
+    LocalDateTime createDate;
+
+    @PrePersist
+    void init() {
+        createDate = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return isActive == user.isActive && Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(name, user.name) && Objects.equals(phoneNumber, user.phoneNumber) && Objects.equals(avatar, user.avatar) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles) && Objects.equals(createDate, user.createDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, name, phoneNumber, isActive, avatar, password, roles, createDate);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+}
---
+++ src/main/java/com/example/ShareTools/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.ShareTools.service;
+
+import com.example.ShareTools.model.User;
+import com.example.ShareTools.model.enums.Role;
+import com.example.ShareTools.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepo;
+
+    public boolean createUser(User user) {
+        String email = user.getEmail();
+        if(userRepo.findByEmail(email) == null) return false;
+        user.setActive(true);
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("User with email: {} created", email);
+        return true;
+    }
+}
