8f46d3af72c615b4914ff0d24799a400974863e5
Lamanxo <smgbysgm@gmail.com>
2023-09-18 21:13:38 +0300

    end of branch

--- src/main/java/com/example/ShareTools/controllers/AdminController.java
+++ src/main/java/com/example/ShareTools/controllers/AdminController.java
@@ -5,6 +5,7 @@ import com.example.ShareTools.model.enums.Role;
 import com.example.ShareTools.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,30 +18,33 @@ import java.util.Map;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class AdminController {
     private final UserService userService;
 
     @GetMapping("/admin")
     public String admin(Model model) {
+        log.warn("Admin method");
         model.addAttribute("users", userService.getAllUsers());
         return "admin";
     }
 
     @PostMapping("/admin/user/ban/{id}")
     public String userBanOrUnban(@PathVariable("id") Long id) {
-        log.info("Ban/Unban user: {}", id);
+        log.warn("Ban/Unban user: {}", id);
         userService.userBanOrUnban(id);
         return "redirect:/admin";
     }
 
     @PostMapping("/admin/user/edit")
-    public String userEdit(@RequestParam("user") User user, @RequestParam Map<String, String> form) {
+    public String userEdit(@RequestParam("userId") User user, @RequestParam Map<String, String> form) {
         userService.changeUserRoles(user, form);
         return "redirect:/admin";
     }
 
-    @GetMapping("/admin/user/edit/{user)")
+    @GetMapping("/admin/user/edit/{user}")
     public String userEdit(@PathVariable("user") User user, Model model) {
+        log.warn("EDIT USER");
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         return "user-edit";
